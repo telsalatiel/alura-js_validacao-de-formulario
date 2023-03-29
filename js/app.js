09b876ea16acb1010d6abba24ded37e13594378b
José Salatiel <josesalatielvieira@gmail.com>
2023-03-29 11:58:13 -0300

    parte 3 finalizada: mensagens customizadas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import { valida } from './validacao'
+import { valida } from './validacao.js'
 
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
-  input.addEventListener('blur', (evento) => {
-    valida(evento.target)
-  })
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
 })
